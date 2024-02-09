6c1da72e958fe21ef427a169683cc180214ba570
Archie-study <archiestudy12@gmail.com>
2024-02-09 22:43:32 +0700

    conditional-rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import Student from "./Student.jsx";
+import UserGreeting from "./UserGreeting.jsx";
 
 function App() {
   return(
     <>
-      <Student name="Archie Bandini" age={28} isStudent={true}/>
-      <Student name="Cantika Debby" age={27} isStudent={false}/>
-      <Student name="Lazuardi Edo" age={24} isStudent={true}/>
-      <Student name="Acarya Mordekhai" age={29} isStudent={false}/>
-      <Student name="Ekhi"/>
+      <UserGreeting  username="Archie"  />
     </>
   );
 }
